fbf0a6b5af4427792c455a05e4e2d7e30ea4926e
win_hyeon_lim <61348734+treepot12@users.noreply.github.com>
2026-01-11 19:21:00 +0900

    첨부파일 테스트 3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {getFirestore} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMBsOREBROZmADYZRuM0lZwW3LOeuWe1k",
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const auth = getAuth(app);
