b44cd87240e7e5eccc7a1f5be70a288c8464f73c
win_hyeon_lim <61348734+treepot12@users.noreply.github.com>
2026-01-11 19:30:52 +0900

    업로드 테스트 5

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAMBsOREBROZmADYZRuM0lZwW3LOeuWe1k",
   authDomain: "test-534af.firebaseapp.com",
   projectId: "test-534af",
-  storageBucket: "test-534af.appspot.com",
+  storageBucket: "test-534af.firebasestorage.app",
   messagingSenderId: "614013525688",
   appId: "1:614013525688:web:d64ddb9d9882bcc99ede4d"
 };
